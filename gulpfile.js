8542601ddf7da1b002a654cdb6ac9961c1d60561
developer <dasx10@gmail.com>
2021-02-22 15:01:41 +0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,4 @@
 const gulp = require('gulp');
-const test = require('./index')
-
-function css(){
-    return gulp.src('./test/src/*.spec.*')
-    .pipe(test())
-    .pipe(gulp.dest('./test/dist'))
+module.exports.t=function(){
+    return gulp.src('./test/src/*.spec.*').pipe(require('./index')()).pipe(gulp.dest('./test/dist'))
 }
-
-module.exports.css = css;
--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const fs = require('fs');
-const animReg = /animation:+[^;]+;/ig;
-let animations = new Set();
-let checkFrames = new Set();
-const getNameAnimation = (animation) => animation.replace(': ',':').split(':')[1].split(' ')[0];
-const findAnimations = (data) => {
+const animReg=/animation:+[^;]+;/ig;
+let animations=new Set();
+let checkFrames=new Set();
+const getNameAnimation=animation=>animation.replace(': ',':').split(':')[1].split(' ')[0];
+const findAnimations=data=>{
     let animation = animReg.exec(data);
     if (animation && animation.length) {
         animations.add(animation[0]);
         findAnimations(data);
-    }
-    return data;
+    }return data;
 }
-
-const gutil = require('gulp-util');
-const through = require('through2');
-module.exports = (options) => through.obj(function(blobFile, enc, cb) {
-    if (blobFile.isNull()) {
+const gutil= require('gulp-util');
+const through=require('through2');
+module.exports=()=>through.obj(function(blobFile,enc,cb) {
+    if (blobFile.isNull()){
         cb(null, blobFile);
         return;
     }
-    if (blobFile.isStream()) {
+    if (blobFile.isStream()){
         cb(new gutil.PluginError('gulp-example-plugin', 'Streaming not supported'));
         return;
     }
@@ -27,18 +24,16 @@ module.exports = (options) => through.obj(function(blobFile, enc, cb) {
     file = findAnimations(file);
     frames = file.split('@keyframes');
     file = frames[0]
-    for(let i = 1; i < frames.length; i ++){
+    for(let i=1;i<frames.length;i++){
         animations.forEach(anim => {
-            let name = getNameAnimation(`${anim}`);
-            name = eval(`/${name} /`);
-            let checkFrame = name.exec(frames[i]);
-            if(checkFrame){
-                checkFrames.add("@keyframes"+checkFrame.input)
-            }
+            let name=getNameAnimation(`${anim}`);
+            name=eval(`/${name}[\{| ]/`);
+            let checkFrame=name.exec(frames[i]);
+            if(checkFrame)checkFrames.add("@keyframes"+checkFrame.input);
         })
     }
     checkFrames.forEach(frame => file += frame);
-    blobFile.contents = new Buffer(file);
+    blobFile.contents=new Buffer(file);
     this.push(blobFile);
     cb();
 });
